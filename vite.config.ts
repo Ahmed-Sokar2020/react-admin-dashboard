ddc36c63c1381827f30b0821cdffbfb260039b56
Ahmed-Sokar2020 <sokar7288@gmail.com>
2026-04-04 18:39:48 +0200

    modefy package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/react-admin-dashboard/",
   plugins: [react(), purgeCss({})],
   build: {
     chunkSizeWarningLimit: 1000,
