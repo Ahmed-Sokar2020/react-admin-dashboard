d3afa93e618ec7ae1290d7e73ab325a61d59a400
Ahmed-Sokar2020 <sokar7288@gmail.com>
2026-04-05 23:24:47 +0200

    modify vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), purgeCss({})],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (
-            id.includes("node_modules/react") ||
-            id.includes("node_modules/react-dom")
-          ) {
-            return "vendor";
-          }
-          if (id.includes("node_modules/recharts")) {
-            return "charts";
-          }
-        },
-      },
-    },
-  },
 });
