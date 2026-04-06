02067cb77596642b89a7bcb87c69ff16c99f45e8
Ahmed-Sokar2020 <sokar7288@gmail.com>
2026-04-06 18:56:41 +0200

    modify vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import purgeCss from "vite-plugin-purgecss";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 import path from "path";
+
 export default defineConfig({
-  plugins: [react(), purgeCss({})],
+  plugins: [
+    react(),
+    purgeCss({}),
+    {
+      name: "copy-404",
+      closeBundle() {
+        const distPath = path.resolve(__dirname, "dist");
+        fs.copyFileSync(
+          path.join(distPath, "index.html"),
+          path.join(distPath, "404.html"),
+        );
+      },
+    },
+  ],
   base: process.env.NODE_ENV === "production" ? "/react-admin-dashboard/" : "/",
   resolve: {
     alias: {
